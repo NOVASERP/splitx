8a78f9423697ae62b19d10753343f277b1b5e8d1
charu-analyst <charuyadav594@gmail.com>
2025-08-24 23:01:30 +0530

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const groupRoutes = require('./routes/groupRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
 
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json"); // 🔥 Firebase service account json
+require("./fireBaseConfig"); // 🔥 Firebase service account json
 
 dotenv.config();
 
@@ -18,10 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.log('MongoDB connection error:', err));
 
-// Firebase initialize
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/groups', groupRoutes);
